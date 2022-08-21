17cdc1bb6079ec4d44366dffae4f4ffad4d816b8
Jp <jyotiprakashpc@outlook.com>
2022-08-21 17:54:31 +0530

    TEst CAse

---
+++ TestCaseUC1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoodAnalyzerProblem
+{
+    [TestClass]
+    public class TestCaseUC1
+    {
+        [TestMethod]
+        public void When_Given_Sad_Message()
+        { 
+            MoodAnalyserUC1 moodAnalyserUC1 = new MoodAnalyserUC1();
+            string result = moodAnalyserUC1.analyseMood("i am sad");
+            Assert.AreEqual(result, "Sad Mood");
+        }
+    }
+}
