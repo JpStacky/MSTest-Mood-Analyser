4e8076a21f3bf8be5ab805ddb1c0fb0216bb96bf
Jp <jyotiprakashpc@outlook.com>
2022-08-21 17:56:08 +0530

    Custom Exception

---
+++ CustomExceptionUC3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class CustomExceptionUC3
+    {
+        public void analyseMood(string message)
+        {
+            string result = "Happy Mood";
+            if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Sad Mood");
+            else if (message.Contains("happy", StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Happy Mood");
+            else
+                throw new MoodAnalysisException("Exception Mood Happy");
+        }
+    }
+}
---
+++ ProgramUC3.cs
@@ -0,0 +1,20 @@
+﻿namespace MoodAnalyzerProblem
+{
+    public class ProgramUC3
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("Enter A Message = ");
+                string message = Console.ReadLine();
+                CustomExceptionUC3 customExceptionUC3 = new CustomExceptionUC3();
+                customExceptionUC3.analyseMood(message);
+            }
+            catch (MoodAnalysisException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
