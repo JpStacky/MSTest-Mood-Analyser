ab7c2cd373dd03894eb5c20fdf6253f22758e55d
Jp <jyotiprakashpc@outlook.com>
2022-08-21 17:33:02 +0530

    Welcome Code

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzerProblem
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Mood Analyzer Problem.");
+        }
+    }
+}
