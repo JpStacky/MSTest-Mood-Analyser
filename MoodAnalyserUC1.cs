6347d2543107aa55c3d4c6e7b4619888ea876a7e
Jp <jyotiprakashpc@outlook.com>
2022-08-21 17:36:48 +0530

    Analyze Mood

---
+++ MoodAnalyserUC1.cs
@@ -0,0 +1,15 @@
+﻿namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyserUC1
+    {
+        public string analyseMood(string message)
+        {
+            string result="Happy Mood";
+            if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+                result = "Sad Mood";
+            else
+                result = "Happy Mood";
+            return result;
+        }
+    }
+}
---
+++ ProgramUC1.cs
@@ -0,0 +1,15 @@
+﻿namespace MoodAnalyzerProblem
+{
+    public class ProgramUC1
+    {
+        public static void Main()
+        {
+            Console.Write("Enter A Message = ");
+            string message = Console.ReadLine();
+
+            MoodAnalyserUC1 moodAnalyserUC1 = new MoodAnalyserUC1();
+            string result = moodAnalyserUC1.analyseMood(message);
+            Console.WriteLine(result);
+        }
+    }
+}
